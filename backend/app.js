e9f035812d4f43aa0090915af69e7d9a0414b301
Ahmed Hussien <ahussien.gomaa@gmail.com>
2021-02-12 13:17:30 +0400

    backend setup

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var config = require('./config');
+console.log(config.trustProxy);
+
+var apiRouter = require('./routes/api');
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api', apiRouter);
+
+module.exports = app;
